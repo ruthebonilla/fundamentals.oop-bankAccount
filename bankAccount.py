6e305c65542c4066a260b250da7fa9654e5c8e9e
ruthebonilla <ruthebonilla01@gmail.com>
2022-02-10 20:19:08 -0500

    python.fund.oop assignments

--- bankAccount.py
+++ bankAccount.py
@@ -16,15 +16,56 @@ class BankAccount:
         return self
 
     def display_account_info(self):
-        print(f"Balance: {self.balance}")
+        print("Balance:" + str(self.balance))
         
     def yield_interest(self):
         if self.balance > 0:
             self.balance += self.balance* self.int_rate
         return self
+    
 
-account1 = BankAccount()
-account2 = BankAccount()
 
-account1.deposit(30).deposit(50).deposit(50).withdraw(50).yield_interest().display_account_info()
-account2.deposit(50).deposit(50).withdraw(30).withdraw(20).withdraw(30).withdraw(30).yield_interest().display_account_info()
+
+# account1 = BankAccount()
+# account2 = BankAccount()
+
+# account1.deposit(30).deposit(50).deposit(50).withdraw(50).yield_interest().display_account_info()
+# account2.deposit(50).deposit(50).withdraw(30).withdraw(20).withdraw(30).withdraw(30).yield_interest().display_account_info()
+
+# /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class User:
+    
+    def __init__(self, name):
+        self.name = name
+        self.account = BankAccount(int_rate = 0.01, balance = 0)
+
+    def make_deposit(self, amount):
+        self.account.deposit(amount)
+        return self
+
+    def make_withdrawl(self, amount):
+        self.account.deposit(amount)
+        return self
+
+    def display_user_balance(self):
+        print("User:" + str(self.name), "Balance: "+ str(self.account.balance))
+        return self
+
+    # def transfer_money(self, amount, animal):
+    #         self.amount -= amount
+    #         animal.amount += amount
+    #         self.display_user_balance()
+    #         animal.display_user_balance()
+    #         return self
+
+user1 = User("Panda")
+user2 = User("Turtle")
+user3 = User("Parrot")
+
+user1.make_deposit(400).make_deposit(100).make_deposit(800).make_withdrawl(200).display_user_balance()
+user2.make_deposit(200).make_deposit(200).make_withdrawl(50).make_withdrawl(50).display_user_balance()
+user3.make_deposit(700).make_withdrawl(100).make_withdrawl(300).make_withdrawl(300).display_user_balance()
+
+# print("*Panda transfers money to his friend Parrot*")
+# user1.transfer_money(300, user3)
--- chainingMethods.py
+++ chainingMethods.py
@@ -6,40 +6,32 @@ class User:
 
     def make_deposit(self, amount):
         self.amount += amount
+        return self
 
     def make_withdrawl(self, amount):
         self.amount -= amount
+        return self
 
     def display_user_balance(self):
         print(f"User: {self.name}, Balance: {self.amount}")
+        return self
 
     def transfer_money(self, amount, animal):
             self.amount -= amount
             animal.amount += amount
             self.display_user_balance()
             animal.display_user_balance()
-        
+            return self
+
+
+
 user1 = User("Panda")
 user2 = User("Turtle")
 user3 = User("Parrot")
 
-user1.make_deposit(400)
-user1.make_deposit(100)
-user1.make_deposit(800)
-user1.make_withdrawl(200)
-user1.display_user_balance()
-
-user2.make_deposit(200)
-user2.make_deposit(200)
-user2.make_withdrawl(50)
-user2.make_withdrawl(50)
-user2.display_user_balance()
-
-user3.make_deposit(700)
-user3.make_withdrawl(100)
-user3.make_withdrawl(300)
-user3.make_withdrawl(300)
-user3.display_user_balance()
+user1.make_deposit(400).make_deposit(100).make_deposit(800).make_withdrawl(200).display_user_balance()
+user2.make_deposit(200).make_deposit(200).make_withdrawl(50).make_withdrawl(50).display_user_balance()
+user3.make_deposit(700).make_withdrawl(100).make_withdrawl(300).make_withdrawl(300).display_user_balance()
 
 print("*Panda transfers money to his friend Parrot*")
 user1.transfer_money(300, user3)
